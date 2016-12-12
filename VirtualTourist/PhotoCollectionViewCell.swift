30aeab14704c9729b5ddf7fe76c9651c25f67cc2
André Brinkop <andre@brinkop.de>
2016-12-12 20:31:31 +0100

    Add connections between the UI and the controller and some small design adjustments

---
+++ VirtualTourist/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PictureCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by André Brinkop on 12.12.16.
+//  Copyright © 2016 André Brinkop. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+}
--- VirtualTourist/PinPhotoViewController.swift
+++ VirtualTourist/PinPhotoViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class PinPhotoViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var emptyCollectionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
