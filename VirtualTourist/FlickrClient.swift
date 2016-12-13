249ba2f6b807475be0983597d2e1985d8ebb03fc
André Brinkop <andre@brinkop.de>
2016-12-13 14:57:05 +0100

    Add HTTPClient to the project

---
+++ VirtualTourist/FlickrClient.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by André Brinkop on 12.12.16.
+//  Copyright © 2016 André Brinkop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FlickrClient {
+
+}
---
+++ VirtualTourist/FlickrConstants.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrConstants.swift
+//  VirtualTourist
+//
+//  Created by André Brinkop on 12.12.16.
+//  Copyright © 2016 André Brinkop. All rights reserved.
+//
+
+import Foundation
+
+extension FlickrClient {
+
+}
